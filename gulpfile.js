e2327781725a440c84e5cbb282195eb4e853a34f
Valentyn <wolf.valentyn@gmail.com>
2023-11-13 20:21:22 +0200

    add watch and build tasks, create default gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,7 @@ const dist = "C:\\MAMP\\htdocs\\react_admin\\admin";
 gulp.task("copy-html", () => {
   return gulp.src("./app/src/index.html") 
               .pipe(gulp.dest(dist))
-})
+});
 
 gulp.task("build-js", () => {
   return gulp.src("./app/src/main.js")
@@ -42,20 +42,32 @@ gulp.task("build-js", () => {
                 }
               }))
               .pipe(gulp.dest(dist))
-})
+});
 
 gulp.task("build-sass", () => {
   return gulp.src("./app/src/scss/style.scss")
               .pipe(sass().on('error', sass.logError))
               .pipe(gulp.dest(dist))
-})
+});
 
 gulp.task("copy-api", () => {
   return gulp.src("./app/api/**/*.*")
               .pipe(gulp.dest(dist + "/api"))
-})
+});
 
 gulp.task("copy-assets", () => {
   return gulp.src("./app/assets/**/*.*")
               .pipe(gulp.dest(dist + "/assets"))
-})
+});
+
+gulp.task("watch", () => {
+  gulp.watch("./app/src/index.html", gulp.parallel("copy-html"));
+  gulp.watch("./app/assets/**/*.*", gulp.parallel("copy-assets"));
+  gulp.watch("./app/api/**/*.*", gulp.parallel("copy-api"));
+  gulp.watch("./app/src/scss/**/*.scss", gulp.parallel("build-sass"));
+  gulp.watch("./app/src/index.html", gulp.parallel("build-js"));
+});
+
+gulp.task("build", gulp.parallel("copy-html", "copy-assets", "copy-api", "build-sass", "build-js"));
+
+gulp.task("default", gulp.parallel("watch", "build"));

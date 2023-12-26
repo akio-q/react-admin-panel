4a53d141e1ce023f3aa2c76455c22a46abb833ed
Valentyn <wolf.valentyn@gmail.com>
2023-12-26 14:37:05 +0200

    delete unnecessary code

--- app/src/main.js
+++ app/src/main.js
@@ -6,26 +6,3 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Editor />
 );
-
-// function getPageList() {
-//   $("h1").remove();
-//   $.get("./api", data => { 
-//     data.forEach(file => {
-//       $("body").append(`<h1>${file}</h1>`)
-//     })
-//   }, "JSON");
-// }
-
-// getPageList();
-
-// $("h1").fadeOut();
-// $("button").click(() => {
-//   $.post("./api/createNewPage.php", { 
-//     "name": $("input").val()
-//   }, () => {
-//     getPageList();
-//   }) 
-//   .fail(() => {
-//     alert("Page already exist!");
-//   })
-// })

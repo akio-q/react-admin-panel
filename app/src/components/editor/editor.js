37afb6c0906516a0870e01d135dadbea1b73f173
Valentyn <wolf.valentyn@gmail.com>
2023-12-26 15:09:05 +0200

    add test app that adds and deletes html pages
    It is a test functionality to try basic requests with php and react. Further on I will use some of this functionality.

--- app/src/components/editor/editor.js
+++ app/src/components/editor/editor.js
@@ -9,13 +9,53 @@ export default class Editor extends Component {
       pageList: [],
       newPageName: ''
     }
+
+    this.createNewPage = this.createNewPage.bind(this);
+  }
+
+  componentDidMount() {
+    this.loadPageList();
+  }
+
+  loadPageList() {
+    axios
+      .get('./api')
+      .then(res => this.setState({pageList: res.data}));
+  }
+
+  createNewPage() {
+    axios
+      .post('./api/createNewPage.php', {'name': this.state.newPageName})
+      .then(res => this.loadPageList())
+      .catch(() => alert("Page already exists!"));
+  }
+
+  deletePage(page) {
+    axios
+      .post('./api/deletePage.php', {'name': page})
+      .then(res => this.loadPageList())
+      .catch(() => alert("Page does not exists!"));
   }
 
   render() {
+    const {pageList} = this.state;
+    const pages = pageList.map((page, i) => {
+      return (
+        <h1 key={i}>{page}
+          <a 
+            href="#"
+            onClick={() => this.deletePage(page)}>(x)</a>
+        </h1>
+      )
+    })
+    
     return (
       <>
-        <input type="text" />
-        <button>Create a page</button>
+        <input 
+          onChange={e => this.setState({newPageName: e.target.value})} 
+          type="text" />
+        <button onClick={this.createNewPage}>Create a page</button>
+        {pages}
       </>
     )
   }

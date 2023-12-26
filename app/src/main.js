7d680587066cd4b562474a44bc1bfcc7cd3c23bc
Valentyn <wolf.valentyn@gmail.com>
2023-12-26 14:22:03 +0200

    add React, basic structure and Editor component

--- app/src/main.js
+++ app/src/main.js
@@ -1,3 +1,31 @@
-import $ from 'jquery';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Editor from './components/editor';
 
-$("h1").fadeOut();
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Editor />
+);
+
+// function getPageList() {
+//   $("h1").remove();
+//   $.get("./api", data => { 
+//     data.forEach(file => {
+//       $("body").append(`<h1>${file}</h1>`)
+//     })
+//   }, "JSON");
+// }
+
+// getPageList();
+
+// $("h1").fadeOut();
+// $("button").click(() => {
+//   $.post("./api/createNewPage.php", { 
+//     "name": $("input").val()
+//   }, () => {
+//     getPageList();
+//   }) 
+//   .fail(() => {
+//     alert("Page already exist!");
+//   })
+// })
